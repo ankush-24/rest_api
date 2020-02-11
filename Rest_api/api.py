8644dec1dcaf02b44ca75ba981f108a4ccf6aba2
ankush-24 <ankush.webllisto@gmail.com>
2020-02-11 20:31:51 +0530

    drf

--- Rest_api/api.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-from drf_user import views
-# from user import views
-
-router = routers.DefaultRouter()
-router.register(r'user',views.UserViewset,basename='user')
-
--- Rest_api/urls.py
+++ Rest_api/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from .api import router
+
+# from .api import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include(router.urls)),
+	path('', include('snippets.urls')),    
 
 ]
